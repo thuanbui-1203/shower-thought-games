fd006b08a383e08eac8828421b5eadaf3a9c7f00
Thuan Bui <thuanbui.dev@gmail.com>
2026-03-18 21:37:22 +0700

    add terrain for lualander

--- Assets/Scenes/Random thoughts/Scripts/Lua Lander/Lander.cs
+++ Assets/Scenes/Random thoughts/Scripts/Lua Lander/Lander.cs
@@ -4,11 +4,13 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Lander : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float turnSpeed = 100f;
+    [SerializeField] private float force = 70f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
     private void Start()
     {
@@ -18,25 +20,21 @@ public class Lander : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        if (Keyboard.current.upArrowKey.isPressed)
+        if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed)
         {
             Debug.Log("up key pressed");
-            Vector3 v3_up = Vector3.up;
-            float force = 70f;
-            rigidbody2D.AddForce(force * Time.deltaTime * v3_up, ForceMode2D.Impulse);
+            _rigidbody2D.AddForce(force * Time.deltaTime * transform.up, ForceMode2D.Impulse);
         }
 
-        if (Keyboard.current.leftArrowKey.isPressed)
+        if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed)
         {
             Debug.Log("left key pressed");
-            float turnSpeed = 100f;
-            rigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            _rigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
         }
-        if (Keyboard.current.rightArrowKey.isPressed)
+        if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed)
         {
             Debug.Log("right key pressed");
-            float turnSpeed = -100f;
-            rigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            _rigidbody2D.AddTorque(turnSpeed * -1 * Time.deltaTime);
         }
     }
     private void Update()
